9758aa01e42513c79afe5b60d13952d278434ef7
Myiah Moore <myiahsade@gmail.com>
2018-08-14 06:49:31 -0500

    check in

---
+++ js.js
@@ -0,0 +1,5 @@
+var person1= {a:"Jimmy Page", b: 62, c:38117};
+
+var person2= {a:"Rick Nielsen",b: 57,c: 61016};
+var person3= {a:"Joe Walsh",b: 58, c:90001};
+var person4= {a:"Lemmy Kilmister",b:57,c:21120};
